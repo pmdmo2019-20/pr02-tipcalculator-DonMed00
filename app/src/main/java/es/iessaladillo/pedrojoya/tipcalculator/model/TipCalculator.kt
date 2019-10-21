1d98ca5018653212663353ce275ed29c38495dc7
DonMeD <xmena97@gmail.com>
2019-10-21 23:18:44 +0200

    21/10 - 1.0

--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
@@ -1,24 +1,37 @@
 package es.iessaladillo.pedrojoya.tipcalculator.model
 
+import kotlin.math.ceil
+
 
 // TipCalculator class. Its constructor receives bill, percentage and diners
 
 class TipCalculator {
+     var bill: Float
+     var percentage: Float
+     var diners: Int
+
+
+    constructor(bill: Float, percentage: Float, diners: Int) {
+        this.bill = bill
+        this.percentage = percentage
+        this.diners = diners
+    }
+
 
     fun calculateTip(): Float {
-        // TODO
+        return (bill * percentage) / 100
     }
 
     fun calculateTotal(): Float {
-        // TODO
+        return bill + calculateTip()
     }
 
     fun calculatePerDiner(): Float {
-        // TODO
+        return calculateTotal() / diners
     }
 
     fun calculatePerDinerRounded(): Float {
-        // TODO
+        return ceil(calculatePerDiner().toDouble()).toFloat()
     }
 
 }
--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
@@ -1,9 +1,64 @@
 package es.iessaladillo.pedrojoya.tipcalculator.ui.main
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import es.iessaladillo.pedrojoya.tipcalculator.R
+import es.iessaladillo.pedrojoya.tipcalculator.model.TipCalculator
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tipCalculator : TipCalculator
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupViews()
+    }
+    //Tengo que lograr que se actualize todo
 
-    // TODO
+    @SuppressLint("ResourceType")
+    private fun setupViews() {
+        txtBill.setText("10")
+        txtPercentage.setText("10.00")
+        txtDiners.setText("1")
+        txtBill.requestFocus()
+        tipCalculator = TipCalculator(
+            txtBill.text.toString().toFloat(),
+            txtPercentage.text.toString().toFloat(),
+            txtDiners.text.toString().toInt()
+        )
+         txtLinks()
+
+         txtOnChanged()
+
+
+    }
+
+    private fun txtLinks() {
+        txtTip.setText(tipCalculator.calculateTip().toString())
+        txtTotal.setText(tipCalculator.calculateTotal().toString())
+        txtPerDiner.setText(tipCalculator.calculatePerDiner().toString())
+        txtPerDinerRounded.setText(tipCalculator.calculatePerDinerRounded().toString())
+    }
+
+    private fun txtOnChanged() {
+        txtBill.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                txtLinks()
+            }
+        })
+    }
+
 
 }
