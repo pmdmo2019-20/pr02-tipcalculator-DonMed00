b201614ca3dd6d2aa33c16d8d52d60d330cab697
DonMeD <xmena97@gmail.com>
2019-10-22 16:52:02 +0200

    22/10 - Ok, except some tests

--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
@@ -5,13 +5,18 @@ import kotlin.math.ceil
 
 // TipCalculator class. Its constructor receives bill, percentage and diners
 
+
 class TipCalculator {
-     var bill: Float
-     var percentage: Float
-     var diners: Int
+     var bill: Float = 0.00f
+     var percentage: Float = 0.00f
+     var diners: Int = 0
 
 
     constructor(bill: Float, percentage: Float, diners: Int) {
+        require(bill >= 0) { "Error, the bill must be a positive number" }
+        require(percentage >= 0) { "Error, the percentage must be a positive number" }
+        require(diners >= 0) { "Error, the diners must be a positive number" }
+
         this.bill = bill
         this.percentage = percentage
         this.diners = diners
--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
@@ -4,105 +4,120 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import es.iessaladillo.pedrojoya.tipcalculator.R
 import es.iessaladillo.pedrojoya.tipcalculator.model.TipCalculator
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
     lateinit var tipCalculator: TipCalculator
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupViews()
+
     }
 
     @SuppressLint("ResourceType")
     private fun setupViews() {
         putEditTextsDefault()
-        txtBill.requestFocus()
         tipCalculator = TipCalculator(
             txtBill.text.toString().toFloat(),
             txtPercentage.text.toString().toFloat(),
             txtDiners.text.toString().toInt()
         )
-        txtLinks()
-        editTextsOnChanged()
+        editTextsOnChanged(txtBill)
+        editTextsOnChanged(txtPercentage)
+        editTextsOnChanged(txtDiners)
 
 
     }
 
     private fun putEditTextsDefault() {
-        txtBill.setText("0")
+        txtBill.setText("0.00")
         txtPercentage.setText("10.00")
         txtDiners.setText("1")
+        txtTip.setText("0.00")
+        txtTotal.setText("0.00")
+        txtPerDiner.setText("0.00")
+        txtPerDinerRounded.setText("0.00")
+
+        txtBill.requestFocus()
     }
 
     private fun txtLinks() {
         txtTip.setText(tipCalculator.calculateTip().toString())
-        txtTotal.setText(tipCalculator.calculateTotal().toString())
+        txtTotal.setText((tipCalculator.calculateTotal()).toString())
         txtPerDiner.setText(tipCalculator.calculatePerDiner().toString())
         txtPerDinerRounded.setText(tipCalculator.calculatePerDinerRounded().toString())
     }
 
-    private fun editTextsOnChanged() {
-        txtBill.addTextChangedListener(object : TextWatcher {
+    private fun editTextsOnChanged(editText: EditText) {
+        editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (txtBill.text.toString() == "") {
-                    txtBill.setText("0")
-                }
-                tipCalculator.bill = txtBill.text.toString().toFloat()
+                checkFields(editText)
+                changeField(editText)
+
                 txtLinks()
             }
         })
-
-        txtPercentage.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (txtPercentage.text.toString() == "") {
-                    txtPercentage.setText("10")
-                }
-                tipCalculator.percentage = txtPercentage.text.toString().toFloat()
-                txtLinks()
-            }
-        })
-        txtDiners.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (txtDiners.text.toString() == "") {
-                    txtDiners.setText("1")
-                }
-                tipCalculator.diners = txtDiners.text.toString().toInt()
-                txtLinks()
-            }
-        })
-
         btnResetTip.setOnClickListener { resetFields(true) }
         btnResetDiners.setOnClickListener { resetFields(false) }
     }
 
+    private fun changeField(editText: EditText) {
+        when (editText) {
+            txtBill ->
+                tipCalculator.bill = editText.text.toString().toFloat()
+
+            txtPercentage ->
+                tipCalculator.percentage = editText.text.toString().toFloat()
+
+            txtDiners ->
+                tipCalculator.diners = editText.text.toString().toInt()
+
+        }
+
+    }
+
+    private fun checkFields(editText: EditText) {
+        when (editText) {
+            txtBill ->
+                if (editText.text.toString() == "") {
+                    editText.setText("0.00")
+                }
+            txtPercentage ->
+                if (editText.text.toString() == "") {
+                    editText.setText("10.00")
+                }
+            txtDiners ->
+                if (editText.text.toString() == "" || editText.text.toString() == ("0")) {
+                    editText.setText("1")
+                }
+
+        }
+
+    }
+
     private fun resetFields(flag: Boolean) {
         if (flag) {
-            txtBill.setText("0")
-            txtTip.setText("0")
-            txtPercentage.setText("10")
-            txtTotal.setText("0")
+            txtBill.setText("0.00")
+            txtTip.setText("0.00")
+            txtPercentage.setText("10.00")
+            txtTotal.setText("0.00")
+            txtBill.requestFocus()
         } else {
             txtDiners.setText("1")
-            txtPerDiner.setText("0")
-            txtPerDinerRounded.setText("0")
+            txtPerDiner.setText("0.00")
+            txtPerDinerRounded.setText("0.00")
+            txtDiners.requestFocus()
         }
     }
 
